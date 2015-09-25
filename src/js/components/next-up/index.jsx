9a82c461c353dab5910deade487d3a9039dbed98
Niklas Lindgren <niklas@sc5.io>
2015-09-25 11:35:03 +0200

    Add next-up component

---
+++ src/js/components/next-up/index.jsx
@@ -0,0 +1,40 @@
+import debug from "debug";
+import React, { Component } from "react";
+import Session from "../session";
+
+const log = debug("schedule:components:next-up");
+
+export class NextUp extends Component {
+    render() {
+        const { getState } = this.props;
+        const { days, time: { today, now } } = getState();
+
+        let currentDay = Object.keys(days).filter(d => today <= new Date(d)).shift();
+        let tracks = Object.keys(days[currentDay].tracks).map(name => ({ name, sessions: days[currentDay].tracks[name] }));
+
+        let nextSessions = tracks.map(t => {
+            return {
+                ...t,
+                next: t.sessions.filter(s => now <= s.start).shift()
+            };
+        });
+
+        return (
+            <div className="next-up">
+                <p>Next up:</p>
+                {
+                    nextSessions.map(t => {
+                        return (
+                            <div className="next-up__session">
+                                <h2>Track: {t.name}</h2>
+                                <Session session={t.next} />
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default NextUp;

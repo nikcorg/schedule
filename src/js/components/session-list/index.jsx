cbfe3226af410d98206a2d2aadb2b78126032164
Niklas Lindgren <niklas@sc5.io>
2015-09-25 18:36:21 +0200

    Refactor day-list view to the normalised model

--- src/js/components/session-list/index.jsx
+++ src/js/components/session-list/index.jsx
@@ -4,50 +4,22 @@ import Session from "../session";
 
 const log = debug("schedule:components:session-list");
 
-const MARGIN = 60 * 5 * 1000; // Display started events for 5 mins
-const MODE_FILTERED = "filtered";
-const MODE_ALL = "all";
 
 export class SessionList extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            mode: MODE_FILTERED
-        };
-    }
-
-    switchMode(mode) {
-        this.setState({ mode });
-    }
-
     render() {
-        const { mode } = this.state;
         const { sessions } = this.props;
-
-        let now = Date.now();
-        let show = 1 < sessions.length && MODE_FILTERED === mode ? sessions.filter(s => now <= Number(s.start) + MARGIN) : sessions;
-        let sessionComps = show.map(s => {
-            return (
-                <div className="session-list__session">
-                    <Session session={s} />
-                </div>
-            );
-        });
 
         return (
             <div className="session-list">
                 {
-                    1 < sessionComps.length &&
-                    <div className="session-list__mode-switcher">
-                        <a href="#" onClick={e => (e.preventDefault(), this.switchMode(MODE_ALL))}
-                            className={mode !== MODE_FILTERED && "active"}>All</a>
-                        {" | "}
-                        <a href="#" onClick={e => (e.preventDefault(), this.switchMode(MODE_FILTERED))} className={mode === MODE_FILTERED && "active"}>Upcoming</a>
-                    </div>
+                    sessions.map(s => {
+                        return (
+                            <div className="session-list__session">
+                                <Session session={s} />
+                            </div>
+                        );
+                    })
                 }
-
-                {sessionComps}
             </div>
         );
     }

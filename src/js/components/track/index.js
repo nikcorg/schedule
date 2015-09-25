30f57a3ee9a4a454e026ccaffc07e19301029adb
Niklas Lindgren <niklas@sc5.io>
2015-09-25 11:05:46 +0200

    Add track collapser control

--- src/js/components/track/index.js
+++ src/js/components/track/index.js
@@ -5,15 +5,38 @@ import SessionList from "../session-list";
 const log = debug("schedule:components:track");
 
 export class Track extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapsed: false
+        };
+    }
+
+    toggleCollapsed() {
+        this.setState({ collapsed: !this.state.collapsed });
+    }
+
     render() {
+        const { collapsed } = this.state;
         const { name, sessions } = this.props;
 
         return (
             <div className="track">
-                <h2>Track: {name}</h2>
-                <div className="track__session-list">
-                    <SessionList sessions={sessions} />
+                <div className="track__header">
+                    <h2>Track: {name}</h2>
+
+                    <div className="track__collapser">
+                        <button onClick={() => this.toggleCollapsed()}>{collapsed ? "+" : "-"}</button>
+                    </div>
                 </div>
+
+                <div className="track__session-list">
+                    {
+                        !collapsed &&
+                        <SessionList sessions={sessions} />
+                    }
+                </div>
+
             </div>
         );
     }

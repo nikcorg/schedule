d728381c3bb461d189e3b893e1b5084c8aa0ca34
Niklas Lindgren <niklas@sc5.io>
2015-09-25 08:19:50 +0200

    Include presenter when set

--- src/js/components/session/index.jsx
+++ src/js/components/session/index.jsx
@@ -1,6 +1,7 @@
 import debug from "debug";
 import React, { Component, PropTypes } from "react";
 import moment from "moment";
+import Presenter from "../presenter";
 
 const log = debug("schedule:components:session");
 
@@ -9,11 +10,16 @@ export class Session extends Component {
         const { session } = this.props;
         return (
             <div className="session">
-                <h4>{session.title}</h4>
+                <h4><a href="{session.link}">{session.title}</a></h4>
                 <div className="session__time">
                     <span className="session__start">{moment(session.start).calendar()}</span>
                     {" "}
                     <span className="session__duration">({session.hasOwnProperty("duration") ? `${session.duration} min` : `o.e.`})</span>
+
+                    {
+                        session.presenter &&
+                        <Presenter presenter={session.presenter} />
+                    }
                 </div>
             </div>
         );

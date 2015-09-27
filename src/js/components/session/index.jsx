44be362bef738397bc3aa10738547ab09896ceb4
Niklas Lindgren <niklas@sc5.io>
2015-09-27 16:05:06 +0300

    Add favourite widget in session component

--- src/js/components/session/index.jsx
+++ src/js/components/session/index.jsx
@@ -1,16 +1,50 @@
 import debug from "debug";
 import React, { Component, PropTypes } from "react";
 import moment from "moment";
+import { getState, updateState } from "../../store";
 import Presenter from "../presenter";
+import Favourite from "../favourite";
 
 const log = debug("schedule:components:session");
 
 export class Session extends Component {
-    render() {
+    resolveIsFavourite(props) {
+        const { session } = props;
+        const { favourites } = getState();
+
+        this.setState({ isFavourite: favourites.sessions.some(id => id === session.id) });
+    }
+
+    componentWillMount() {
+        this.resolveIsFavourite(this.props);
+    }
+
+    componentWillReceiveProps(newProps) {
+        this.resolveIsFavourite(newProps);
+    }
+
+    toggleFavourite() {
+        const { isFavourite } = this.state;
+        const { favourites } = getState();
         const { session } = this.props;
+
+        if (isFavourite) {
+            updateState("favourites", { sessions: favourites.sessions.filter(id => id !== session.id) });
+        } else {
+            updateState("favourites", { sessions: [...favourites.sessions, session.id] });
+        }
+    }
+
+    render() {
+        const { isFavourite } = this.state;
+        const { session } = this.props;
+
         return (
-            <div className="session">
+            <div className={"session " + (isFavourite ? "session--favourite" : "")}>
                 <h4><a href={session.link}>{session.title}</a></h4>
+                <div class="session__favourite">
+                    <Favourite selected={isFavourite} onClick={() => this.toggleFavourite() } />
+                </div>
                 <div className="session__time">
                     <span className="session__start">{moment(session.start).calendar()}</span>
                     {" "}

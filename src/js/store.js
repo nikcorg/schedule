1bea1b9f148031249e7c0cadd1b8d4dc86ea11dc
Niklas Lindgren <niklas@sc5.io>
2015-09-27 16:22:23 +0300

    Add subscription mechanism to store

--- src/js/store.js
+++ src/js/store.js
@@ -1,5 +1,27 @@
 let currentState = {};
 
+let globalListeners = [];
+let branchListeners = {};
+
+let notify = callback => {
+    try {
+        callback(getState);
+    } catch (e) {}
+};
+
+export function onUpdate(branch, callback) {
+    if (null == callback && "function" == typeof branch) {
+        // The callback is passed in branch for global listeners
+        globalListeners.push(branch);
+    }
+
+    if (!branchListeners.hasOwnProperty("branch")) {
+        branchListeners[branch] = [];
+    }
+
+    branchListeners[branch].push(callback);
+}
+
 export function getState() {
     return { ...currentState };
 }
@@ -10,6 +32,12 @@ export function updateState(branch, patch) {
     } else {
         currentState = { ...currentState, [branch]: { ...currentState[branch], ...patch } };
     }
+
+    if (branchListeners.hasOwnProperty(branch)) {
+        branchListeners[branch].forEach(notify);
+    }
+
+    globalListeners.forEach(notify);
 
     return true;
 }
